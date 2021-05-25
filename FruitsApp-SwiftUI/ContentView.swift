a66e04e1e9b92e8f0d1de1d87f842a9227007819
Justyna <kowalkowska.just@gmail.com>
2021-05-25 22:27:12 +0200

    Setting up project.

---
+++ FruitsApp-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FruitsApp-SwiftUI
+//
+//  Created by Justyna Kowalkowska on 25/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FruitsApp-SwiftUI/FruitsApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp_SwiftUIApp.swift
+//  FruitsApp-SwiftUI
+//
+//  Created by Justyna Kowalkowska on 25/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
