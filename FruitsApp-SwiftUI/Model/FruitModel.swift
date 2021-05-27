8498e537cfe6eef105c6a72f93304c55ed7bf139
Justyna <kowalkowska.just@gmail.com>
2021-05-27 23:34:11 +0200

    Creating a Data Model of Fruit and Data of Fruit.

---
+++ FruitsApp-SwiftUI/Model/FruitModel.swift
@@ -0,0 +1,20 @@
+//
+//  FruitModel.swift
+//  FruitsApp-SwiftUI
+//
+//  Created by Justyna Kowalkowska on 27/05/2021.
+//
+
+import SwiftUI
+
+//MARK: - Fruits Data Model
+
+struct Fruit: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+}
