70a8d9cc0f86298785f6a2d045b44e0bd839ec14
Justyna <kowalkowska.just@gmail.com>
2021-05-28 13:48:21 +0200

    Creating a ListView in the ContentView and retrive data using ForEach loop.

--- FruitsApp-SwiftUI/App/ContentView.swift
+++ FruitsApp-SwiftUI/App/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: - Properties
+    
+    var fruits: [Fruit] = fruitsData
+    
+    //MARK: - Body
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(fruits) { fruit in
+                    FruitRowView(fruit: fruit)
+                        .padding(.vertical, 4)
+                }
+            } //: List
+            .navigationTitle("Fruits")
+        } //: Navigation
     }
 }
 
+//MARK: - Preview
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(fruits: fruitsData)
     }
 }
