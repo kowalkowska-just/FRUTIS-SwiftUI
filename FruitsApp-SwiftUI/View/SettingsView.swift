f03c06521f29d9f4220f3f47c0dda5cc0d63ea3b
Justyna <kowalkowska.just@gmail.com>
2021-06-08 00:18:08 +0200

    Creating a reusable SettingLabelView for SettingsView.

--- FruitsApp-SwiftUI/View/SettingsView.swift
+++ FruitsApp-SwiftUI/View/SettingsView.swift
@@ -23,13 +23,20 @@ struct SettingsView: View {
                     
                     GroupBox(
                         label:
-                            HStack {
-                                Text("Fructus".uppercased()).fontWeight(.bold)
-                                Spacer()
-                                Image(systemName: "info.circle")
-                            }
+                           SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
                     ) {
-                        Text("Content")
+                        Divider().padding(.vertical, 4)
+                        
+                        HStack(alignment: .center, spacing: 10) {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(9)
+                            
+                            Text("Most fruits are naturally low in fat, sodium, and calories. None have cholesterol. Fruits are sources of many essentail nutrients, including potassium, dietary fiber, vitamins, and much more.")
+                                .font(.footnote)
+                        }
                     }
                     
                     //MARK: - Section 2
