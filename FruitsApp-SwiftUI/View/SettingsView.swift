97dd23ee9a11eed110b767c331f77dce84f99e24
Justyna <kowalkowska.just@gmail.com>
2021-06-14 00:08:06 +0200

    Creating a second section in SettingView and adding a toggle to see the welcome view again.

--- FruitsApp-SwiftUI/View/SettingsView.swift
+++ FruitsApp-SwiftUI/View/SettingsView.swift
@@ -12,6 +12,7 @@ struct SettingsView: View {
     //MARK: - Properties
     
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
     
     //MARK: - Body
     
@@ -40,6 +41,35 @@ struct SettingsView: View {
                     }
                     
                     //MARK: - Section 2
+                    
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")) {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(
+                            Color(UIColor.tertiarySystemBackground)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        )
+                    }
                     
                     //MARK: - Section 3
                     
